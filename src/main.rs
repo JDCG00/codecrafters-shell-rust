c79023edbb09146df2385e913e8e8ee066422dc7
jdcg <>
2026-03-29 20:51:24 +0200

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -9,14 +9,31 @@ fn main() {
         let mut command = String::new();
         io::stdin().read_line(&mut command).unwrap();
         command = command.trim().to_string();
-        if command == "exit" {
-            break;
-        }
+        // if command == "exit" {
+        //     break;
+        // }
 
-        if command.starts_with("echo ") {
-            println!("{}", &command[5..]);
-        } else {
-            println!("{}: command not found", command.trim());
+        match command.split_once(' ') {
+            Some((command, arguments)) => match command {
+                "echo" => println!("{}", arguments),
+                "type" => match arguments {
+                    "echo" => println!("{} is a shell builtin", arguments),
+                    "type" => println!("{} is a shell builtin", arguments),
+                    "exit" => println!("{} is a shell builtin", arguments),
+                    _ => println!("{}: not found", arguments),
+                },
+                _ => println!("{}: command not found", command.trim()),
+            },
+            None => match command.as_str() {
+                "exit" => break,
+                "echo" => println!(),
+                _ => println!("{}: command not found", command.trim()),
+            },
         }
+        // if command.starts_with("echo ") {
+        //     println!("{}", &command[5..]
+        // } else {
+        //     println!("{}: command not found", command.trim());
+        // }
     }
 }

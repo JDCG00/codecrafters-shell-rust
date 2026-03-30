4c7d71128ef6ade3358af4fa00da8f5738636bcb
jdcg <>
2026-03-30 21:14:39 +0200

    avanzando exec perms

--- src/main.rs
+++ src/main.rs
@@ -46,13 +46,19 @@ fn read_perms(dir: &str, argument: &str) -> io::Result<()> {
         let file = entry.file_name();
         let path = entry.path();
         let permissions = entry.metadata()?.permissions().mode();
+        let is_exec = (permissions & 0o111) != 0;
         if file == argument {
             println!(
-                "Binario {} encontrado en {} con permisos: {:o}",
+                "Binario {} encontrado en {} con permisos ejecutables: {} ({:o})",
                 argument,
                 path.display(),
-                permissions
+                is_exec,
+                permissions & 0o777
             );
+
+            if is_exec {
+                println!("{} is {}", file.display(), path.display())
+            }
         }
     }
     Ok(())
